3532bea912b032a78fd2a6eb17790aca0427d805
JuanManuel2001 <j.rodriguezd@uniandes.edu.co>
2021-03-17 20:40:41 -0500

    primera entrega reto 2

--- App/model.py
+++ App/model.py
@@ -1,41 +1,57 @@
-﻿"""
- * Copyright 2020, Departamento de sistemas y Computación,
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contribuciones:
- *
- * Dario Correal - Version inicial
- """
-
-
-import config as cf
+﻿import config as cf
 from DISClib.ADT import list as lt
 from DISClib.ADT import map as mp
 from DISClib.DataStructures import mapentry as me
-from DISClib.Algorithms.Sorting import shellsort as sa
 assert cf
 
-"""
-Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
-los mismos.
-"""
+def newCatalog():
+    catalog = {'title': None,
+                'channel_title': None,
+                'views': None,
+                'likes': None,
+                'dislikes': None,
+                "categoria":None}
+
+    catalog['title'] = lt.newList('SINGLE_LINKED', compareVideosIds)
+    catalog['channel_title'] = lt.newList('SINGLE_LINKED', compareVideosIds)
+    catalog['views'] = mp.newMap(375943,
+                                maptype='CHAINING',
+                                loadfactor=4.0,
+                                comparefunction=compareVideosIds)
+    catalog['likes'] = mp.newMap(375943,
+                                   maptype='CHAINING',
+                                   loadfactor=4.0,
+                                   comparefunction=compareVideosIds)
+    catalog['dislikes'] = mp.newMap(375943,
+                                   maptype='CHAINING',
+                                   loadfactor=4.0,
+                                   comparefunction=compareVideosIds)
+    catalog["categoria"] = mp.newMap(375943,
+                                   maptype='CHAINING',
+                                   loadfactor=4.0,
+                                   comparefunction=compareVideosIds)
+    return catalog
+
+def addVideoDetails(catalog, video):
+    lt.addLast(catalog['title'], video)
+    mp.put(catalog['title'], video["video_id"], video)
+def addMovieCasting(catalog, video):
+    lt.addLast(catalog['channel_title'],video)
+    mp.put(catalog['channel_title'], video['video_id'], video)
+def compareVideosIds(id1, id2):
+    if (int(id1) == int(id2)):
+        return 0
+    elif int(id1) > int(id2):
+        return 1
+    else:
+        return -1
+def requerimiento1(categoria,catalog,n):
+    valor_a_busar=[]
+    for i in categoria["categorias"]["elements"]:
+        for k,v in i.items():
+            if categoria in v:
+                valor_a_busar.append(v)
+    valor=valor_a_busar[0][0:2]
 
 # Construccion de modelos
 
--- App/view.py
+++ App/view.py
@@ -1,31 +1,11 @@
-﻿"""
- * Copyright 2020, Departamento de sistemas y Computación, Universidad
- * de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- """
-
-import config as cf
+﻿import config as cf
 import sys
 import controller
 from DISClib.ADT import list as lt
 assert cf
-
+from time import process_time 
+from DISClib.ADT import map as mp
+from DISClib.DataStructures import mapentry as me
 
 """
 La vista se encarga de la interacción con el usuario
@@ -34,12 +14,24 @@ se hace la solicitud al controlador para ejecutar la
 operación solicitada
 """
 
+def initCatalo():
+    return controller.initcatalog()
+def intiCategoria():
+    return controller.intiCategoria()
+
+def loadData():
+    controller.loaddata(catalog)
+
 def printMenu():
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("2- Videos por categoria y pais ")
+    print("3- Video tendencia por pais")
+    print("4- Video tendencia por categoria")
+    print ("5- Video por mas likes")
 
 catalog = None
+Categoria=None
 
 """
 Menu principal
@@ -49,10 +41,8 @@ while True:
     inputs = input('Seleccione una opción para continuar\n')
     if int(inputs[0]) == 1:
         print("Cargando información de los archivos ....")
-
-    elif int(inputs[0]) == 2:
-        pass
-
-    else:
-        sys.exit(0)
+        catalog = controller.initcatalog()
+        #print("Se cargo la informacion del catalogo")
+        #print("se cargaron:" +str(lt.size(catalog["videos"]))+ "videos")
+        print(catalog["views"])
 sys.exit(0)
